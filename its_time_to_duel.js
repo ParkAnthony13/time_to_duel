a46443de93d982a2a06685b99f5b0f86e6934b40
ParkAnthony13 <anth0nypark13@gmail.com>
2021-07-06 16:11:41 -0700

    correction made

--- its_time_to_duel.js
+++ its_time_to_duel.js
@@ -35,7 +35,11 @@ class Effect extends Card {
         if( target instanceof Unit ) {
             if (this.stat == "resilience") {
                 target.res += this.magnitude;
-                console.log(`${target.name} Mag: ${this.magnitude} Resilience: ${target.res}`)
+                // console.log(`${target.name} Mag: ${this.magnitude} Resilience: ${target.res}`);
+            }
+            if (this.stat == "power") {
+                target.power += this.magnitude;
+                // console.log(`${target.name} Mag: ${this.magnitude} Power: ${target.power}`);
             }
         } else {
             throw new Error( "Target must be a unit!" );
@@ -44,22 +48,41 @@ class Effect extends Card {
 }
 
 //make an instance of red belt ninja
+console.log("step 1");
 const rbNinja = new Unit("Red Belt Ninja",3,3,4);
 console.log(rbNinja);
+
+
 //make an instance of hard algo and play it on red belt ninja
+console.log("step 2");
 const hardAlg = new Effect("Hard Algorithm",2,"increase target's resilience by 3","resilience",3)
 hardAlg.play(rbNinja);
+console.log(rbNinja);
+
+
 //make an instance of black belt ninja
+console.log("step 3");
 const bbNinja = new Unit("Black Belt Ninja",4,5,4);
 console.log(bbNinja);
+
+
 //make an instance of unhandled promise rejection and play it on redbelt ninja
+console.log("step 4")
 const upr = new Effect("Unhandled Promise Rejection",1,"reduce target's resilience by 2","resilience",-2)
 upr.play(rbNinja);
+console.log(rbNinja);
+
+
 //make an instance of pair programming and plaiy it on red belt ninja
+console.log("step 5");
 const pp = new Effect("Pair Programming",3,"increase target's power by 2","power",2)
 pp.play(rbNinja);
+console.log(rbNinja);
+
+
 //red belt ninja uses attack method on black belt ninja
+console.log("step 6");
 rbNinja.attack(bbNinja);
-console.log(rbNinja)
-console.log(bbNinja)
+console.log(rbNinja);
+console.log(bbNinja);
 
